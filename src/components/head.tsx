762a9e34d24fad87a7af0cf206da3a141764922c
finneganz <abs0lutely.n0nsense.cs@gmail.com>
2021-10-27 17:11:54 +0900

    initial commit

---
+++ src/components/head.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head"
+
+const Header = () => {
+    return (
+        <div>
+            <Head>
+                <title>TwitterClone | </title>
+                <meta name="description" content="TwitterClone" />
+            </Head>
+            <h1>TwitterClone</h1>
+        </div>
+    )
+}
+
+export default Header;
---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import { AppProps } from "next/app"
+import Header from "../components/head"
+
+const Home = () => {
+    return (
+        <div>
+            <Header></Header>
+            
+        </div>
+    )
+}
+
+export default Home;
